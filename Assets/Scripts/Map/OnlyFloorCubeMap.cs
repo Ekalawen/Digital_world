3b3ee5ff5e9cee5e4bb360eb705308d3373586e9
Ekalawen <alexys.dussier@gmail.com>
2020-03-08 19:32:14 +0100

    équilibrage du level RedWire

--- Assets/Scripts/Map/OnlyFloorCubeMap.cs
+++ Assets/Scripts/Map/OnlyFloorCubeMap.cs
@@ -13,7 +13,7 @@ public class OnlyFloorCubeMap : CubeMap {
         // On crée le sol de la map !
         if(isBrisableFloor)
             currentCubeTypeUsed = Cube.CubeType.SPECIAL;
-        Mur floor = new Mur(Vector3.zero, Vector3.right, tailleMap.x, Vector3.forward, tailleMap.y);
+        Mur floor = new Mur(Vector3.zero, Vector3.right, tailleMap.x + 1, Vector3.forward, tailleMap.y + 1);
 
         // On veut générer des caves dangeureuses :3
         // Qui possèderont des lumières !
@@ -49,9 +49,9 @@ public class OnlyFloorCubeMap : CubeMap {
                 Random.Range(2, tailleMap.y - sizeCave.y),
                 Random.Range(2, tailleMap.z - sizeCave.z));
         } else {
-            return new Vector3(Random.Range(0, tailleMap.x - sizeCave.x + 1),
-                Random.Range(1, tailleMap.y - sizeCave.y + 1), // On autorize pas la cave à être complètement au sol !
-                Random.Range(0, tailleMap.z - sizeCave.z + 1));
+            return new Vector3(Random.Range(0, tailleMap.x - sizeCave.x + 2),
+                Random.Range(1, tailleMap.y - sizeCave.y + 2), // On autorize pas la cave à être complètement au sol !
+                Random.Range(0, tailleMap.z - sizeCave.z + 2));
         }
     }
 }
