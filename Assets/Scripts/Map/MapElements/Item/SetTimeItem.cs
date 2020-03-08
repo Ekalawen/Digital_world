5f5928a60aaec881ac9fa15cc34a1b929ba8e083
Ekalawen <alexys.dussier@gmail.com>
2020-03-08 13:06:20 +0100

    ajout du setTimeItem

---
+++ Assets/Scripts/Map/MapElements/Item/SetTimeItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTimeItem : Item {
+
+    public float settedTime = 25.0f;
+
+    public override void OnTrigger(Collider hit) {
+        float currentTime = gm.timerManager.GetRemainingTime();
+        float timeToRemove = settedTime - currentTime;
+        gm.timerManager.AddTime(timeToRemove);
+    }
+}
