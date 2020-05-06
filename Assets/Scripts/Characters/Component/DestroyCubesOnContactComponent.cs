ac2d26b00a59aa7dda526295d598c22da97ccd5d
Ekalawen <alexys.dussier@gmail.com>
2020-05-06 19:38:53 +0200

    Ajout des sondes Big !!! :D

---
+++ Assets/Scripts/Characters/Component/DestroyCubesOnContactComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCubesOnContactComponent : MonoBehaviour {
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        Cube cube = hit.gameObject.GetComponent<Cube>();
+        if (cube != null) {
+            cube.Explode();
+        }
+    }
+}
