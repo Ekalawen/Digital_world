7d7623ac79be41b5ff1741cf609df86adc46779f
Ekalawen <alexys.dussier@gmail.com>
2020-05-08 15:04:43 +0200

    Add GoToDirectionController

---
+++ Assets/Scripts/Characters/Controllers/GoToDirectionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToDirectionController : IController {
+
+    public Vector3 direction;
+
+    protected override void UpdateSpecific() {
+        Vector3 pointToGo = transform.position + direction.normalized * vitesse * 2;
+        Move(pointToGo);
+    }
+
+    public override bool IsInactive() {
+        return false;
+    }
+
+    public override bool IsMoving() {
+        return true;
+    }
+}
