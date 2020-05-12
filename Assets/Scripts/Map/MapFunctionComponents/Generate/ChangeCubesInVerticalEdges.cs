26f88da12b3032a7c9f6536510a866925ba691fb
Ekalawen <alexys.dussier@gmail.com>
2020-05-12 23:42:23 +0200

    Les coins dans Inside the Transistors sont maintenant des cubes de la mort ! :)

---
+++ Assets/Scripts/Map/MapFunctionComponents/Generate/ChangeCubesInVerticalEdges.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCubesInVerticalEdges : GenerateCubesMapFunction {
+
+    public int edgesOffset = 1;
+
+    public override void Activate() {
+        List<Cube> cubes = map.GetAllCubes();
+        foreach(Cube cube in cubes) {
+            Vector3 pos = cube.transform.position;
+            if(IsInVerticalEdges(pos, edgesOffset)) {
+                map.DeleteCube(cube);
+                map.AddCube(pos, cubeType);
+            }
+        }
+    }
+
+    public bool IsInVerticalEdges(Vector3 pos, int offset = 1) {
+        pos = MathTools.Round(pos);
+        int nbEdges = 0;
+        nbEdges += (pos.x <= offset || pos.x >= map.tailleMap.x - offset) ? 1 : 0;
+        nbEdges += (pos.z <= offset || pos.z >= map.tailleMap.z - offset) ? 1 : 0;
+        return pos.y != 0 && pos.y != map.tailleMap.y
+            && nbEdges == 2;
+    }
+}
