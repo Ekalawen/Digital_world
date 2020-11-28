81213b8c4b3c8afaab7b5e26323adbfe5e6e5455
Ekalawen <alexys.dussier@gmail.com>
2020-11-28 15:18:23 +0100

    Create the Tooltip and TooltipActivator classes :)

---
+++ Assets/Scripts/UI/Tooltip/Tooltip.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour {
+
+    protected static Tooltip instance;
+
+    public RectTransform background;
+    public TMPro.TMP_Text text;
+
+    public void Awake() {
+        instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void Update() {
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            transform.parent.GetComponent<RectTransform>(), 
+            Input.mousePosition, 
+            null, 
+            out localPoint);
+        transform.localPosition = localPoint;
+    }
+
+    protected void ShowProtected(string message) {
+        gameObject.SetActive(true);
+        text.text = message;
+        background.sizeDelta = text.GetPreferredValues(message);
+    }
+
+    protected void HideProtected() {
+        gameObject.SetActive(false);
+    }
+
+    public static void Show(string message) {
+        instance.ShowProtected(message);
+    }
+
+    public static void Hide() {
+        instance.HideProtected();
+    }
+}
---
+++ Assets/Scripts/UI/Tooltip/TooltipActivator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipActivator : MonoBehaviour {
+
+    public string message = "TooltipActivatorMessage";
+
+    protected RectTransform trigger;
+    protected bool wasContaining = false;
+
+    public void Start() {
+        trigger = GetComponent<RectTransform>();
+        if (trigger == null)
+            Debug.LogWarning("Un TooltipActivator doit être placé sur un GameObject avec un RectTransform ! ;)");
+    }
+
+    public void Update() {
+        bool currentlyContaining = RectTransformUtility.RectangleContainsScreenPoint(trigger, Input.mousePosition);
+        if(currentlyContaining && !wasContaining) {
+            Tooltip.Show(message);
+        }
+        if(!currentlyContaining && wasContaining) {
+            Tooltip.Hide();
+        }
+        wasContaining = currentlyContaining;
+    }
+}
