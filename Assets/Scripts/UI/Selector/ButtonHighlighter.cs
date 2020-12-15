e85c290922ebbbdae0a4f9335289aead0d85d9ff
Ekalawen <alexys.dussier@gmail.com>
2020-12-15 13:37:21 +0100

    Create ButtonHighlighter component :)

---
+++ Assets/Scripts/UI/Selector/ButtonHighlighter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHighlighter : MonoBehaviour {
+
+    public float intervalTime = 0.5f;
+    public float timeToBounceSize = 0.033f;
+    public float timeToNormalSize = 0.166f;
+    public float bounceSize = 1.125f;
+
+    protected Timer timer;
+    protected RectTransform rect;
+
+    void Start() {
+        timer = new Timer(intervalTime);
+        rect = GetComponent<RectTransform>();
+        if(rect == null) {
+            Debug.LogWarning("Un ButtonHighlighter doit posséder un RectTransform ! ;)");
+        }
+    }
+
+    void Update() {
+        if (timer.IsOver()) {
+            StartCoroutine(CBounce());
+            timer.Reset();
+        }
+    }
+
+    protected IEnumerator CBounce() {
+        Timer toBounceTimer = new Timer(timeToBounceSize);
+        Vector3 startScale = rect.localScale;
+        while (!toBounceTimer.IsOver()) {
+            float avancement = toBounceTimer.GetAvancement();
+            rect.localScale = startScale * (1 + (bounceSize - 1) * avancement);
+            yield return null;
+        }
+        Timer toNormalTimer = new Timer(timeToNormalSize);
+        while (!toNormalTimer.IsOver())
+        {
+            float avancement = toNormalTimer.GetAvancement();
+            rect.localScale = startScale * (1 + (bounceSize - 1) * (1 - avancement));
+            yield return null;
+        }
+        rect.localScale = startScale;
+    }
+}
