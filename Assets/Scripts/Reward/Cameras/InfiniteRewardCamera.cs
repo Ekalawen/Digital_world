7300ff29b1d0778828bb1c4c7272be2cb7df5176
Ekalawen <alexys.dussier@gmail.com>
2020-12-16 16:45:06 +0100

    Adjust parameters of InfiniteRewardCamera ! <3

--- Assets/Scripts/Reward/Cameras/InfiniteRewardCamera.cs
+++ Assets/Scripts/Reward/Cameras/InfiniteRewardCamera.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class InfiniteRewardCamera : RewardCamera {
 
     public float playerTimeOffset = 5f;
-    public float verticalOffset = 5f;
-    public float minVerticalOffset = 0.25f;
+    public float verticalOffsetCoef = 5f;
+    public AnimationCurve verticalOffsetCurve;
+    public AnimationCurve minVerticalOffsetCurve;
+    public AnimationCurve smoothingCurve;
 
     protected RewardTrailThemeDisplayer playerDisplayer;
+    protected float minVerticalOffset;
 
     public override void Initialize() {
         base.Initialize();
         playerDisplayer = rm.GetPlayerDisplayer();
+        minVerticalOffset = minVerticalOffsetCurve.Evaluate(playerDisplayer.GetAcceleration());
+        transform.position = playerDisplayer.GetCurve().GetAvancement(0.0f);
     }
 
     public override void Update() {
@@ -21,9 +26,7 @@ public class InfiniteRewardCamera : RewardCamera {
         transform.LookAt(currentPlayerPosition, Vector3.up);
 
         Vector3 playerPositionOffseted = GetPlayerPositionOffseted();
-        Vector3 newPosition = (transform.position + playerPositionOffseted) / 2;
-        newPosition.x = playerPositionOffseted.x;
-        transform.position = newPosition;
+        transform.position = playerPositionOffseted;
     }
 
     protected Vector3 GetCurrentPlayerPosition() {
@@ -36,14 +39,45 @@ public class InfiniteRewardCamera : RewardCamera {
     }
 
     protected Vector3 GetPlayerPositionOffseted() {
-        Curve curve = playerDisplayer.GetCurve();
+        Vector3 timeOffsetedPosition = GetTimeOffsetedPlayerPosition();
+        Vector3 verticallyOffsetedPosition = VerticallyOffsetPosition(timeOffsetedPosition);
+        Vector3 smoothedPosition = SmoothPosition(verticallyOffsetedPosition);
+
         Timer timer = playerDisplayer.GetDurationTimer();
         float acceleration = playerDisplayer.GetAcceleration();
-        float rewardPlayerTimeOffset = playerTimeOffset * acceleration * (1 - timer.GetAvancement());
+        float rewardPlayerTimeOffset = playerTimeOffset * acceleration * Mathf.Clamp01(1 - timer.GetAvancement());
+        float currentVerticalOffset = Mathf.Max(minVerticalOffset, verticalOffsetCoef * verticalOffsetCurve.Evaluate(timer.GetAvancement()));
+        float smoothingCoef = smoothingCurve.Evaluate(timer.GetAvancement());
+        Debug.Log($"timeOffset = {rewardPlayerTimeOffset} verticalOffset = {currentVerticalOffset} smmothingCoef = {smoothingCoef}");
+
+        return smoothedPosition;
+    }
+
+    protected Vector3 SmoothPosition(Vector3 position) {
+        Timer timer = playerDisplayer.GetDurationTimer();
+        float smoothingCoef = smoothingCurve.Evaluate(timer.GetAvancement());
+        Vector3 smoothedPosition = transform.position * smoothingCoef + position * (1 - smoothingCoef);
+        smoothedPosition.x = position.x;
+        return smoothedPosition;
+    }
+
+    protected Vector3 VerticallyOffsetPosition(Vector3 position) {
+        Timer timer = playerDisplayer.GetDurationTimer();
+        float currentVerticalOffset = Mathf.Max(minVerticalOffset, verticalOffsetCoef * verticalOffsetCurve.Evaluate(timer.GetAvancement()));
+        Vector3 verticallyOffsetedPosition = position + Vector3.up * currentVerticalOffset;
+        return verticallyOffsetedPosition;
+    }
+
+    protected Vector3 GetTimeOffsetedPlayerPosition() {
+        Timer timer = playerDisplayer.GetDurationTimer();
+        Curve curve = playerDisplayer.GetCurve();
+        float acceleration = playerDisplayer.GetAcceleration();
+
+        float rewardPlayerTimeOffset = playerTimeOffset * acceleration * Mathf.Clamp01(1 - timer.GetAvancement());
         float offsetedTime = Mathf.Max(0, timer.GetElapsedTime() - rewardPlayerTimeOffset);
         float offsetedAvancement = offsetedTime / timer.GetDuree();
-        Vector3 offsetedPosition = curve.GetAvancement(offsetedAvancement);
-        float currentVerticalOffset = Mathf.Max(minVerticalOffset, verticalOffset * (1 - timer.GetAvancement()));
-        return offsetedPosition + Vector3.up * currentVerticalOffset;
+
+        Vector3 timeOffsetedPosition = curve.GetAvancement(offsetedAvancement);
+        return timeOffsetedPosition;
     }
 }
