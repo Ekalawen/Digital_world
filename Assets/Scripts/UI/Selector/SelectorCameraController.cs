2b68a0bc9ac7eb0f0fadc5bcd130ac9d8fe7b56f
Ekalawen <alexys.dussier@gmail.com>
2020-12-17 15:34:45 +0100

    Really increase and fluidify mouvement of the SelectorCamera ! <3

--- Assets/Scripts/UI/Selector/SelectorCameraController.cs
+++ Assets/Scripts/UI/Selector/SelectorCameraController.cs
@@ -9,6 +9,9 @@ public class SelectorCameraController : MonoBehaviour {
     public Vector3 speedMouse;
     public Vector3 speedKeyboard;
     public float dureeRotations = 0.3f;
+    public float speedLookAtCenter = 10;
+    public float dureeBeforeFocusToInterestPoint = 0.1f;
+    public float mouseSmoothing = 0.3f;
 
     [Header("ElasticitySphere")]
     public Vector2 sizeElasticitySphere;
@@ -21,10 +24,15 @@ public class SelectorCameraController : MonoBehaviour {
 
     protected Vector3 lastClosestLevelPosition = Vector3.zero;
     protected bool isMoving;
+    protected Timer lastIsMovingTimer;
     protected Coroutine rotationCoroutine = null;
+    protected Coroutine lookAtCentralProjectionCoroutine = null;
+    protected float oldMouseSpeedX = 0;
+    protected float oldMouseSpeedY = 0;
 
     public void Start() {
         speedMouse *= 100;
+        lastIsMovingTimer = new Timer(dureeBeforeFocusToInterestPoint);
     }
 
     public void Update() {
@@ -39,28 +47,84 @@ public class SelectorCameraController : MonoBehaviour {
          || selectorManager.HasSelectorPathUnlockScreenOpen())
             return;
 
-        float speedX = Input.GetAxis("Horizontal") * speedKeyboard.x;
+        float speedX = Input.GetAxis("HorizontalFullGravity") * speedKeyboard.x;
         float speedY = Input.GetAxis("Depth") * speedKeyboard.y;
         float speedZ = Input.GetAxis("Vertical") * speedKeyboard.z;
 
-        if(Input.GetMouseButton(0)) {
-            speedX += - Input.GetAxis("Mouse X") * speedMouse.x;
-            speedY += - Input.GetAxis("Mouse Y") * speedMouse.y;
+        if (Input.GetMouseButton(0)) {
+            float mouseSpeedX = -Input.GetAxis("Mouse X") * (1 - mouseSmoothing) + oldMouseSpeedX * mouseSmoothing;
+            float mouseSpeedY = -Input.GetAxis("Mouse Y") * (1 - mouseSmoothing) + oldMouseSpeedY * mouseSmoothing;
+            oldMouseSpeedX = mouseSpeedX;
+            oldMouseSpeedY = mouseSpeedY;
+            speedX += mouseSpeedX * speedMouse.x;
+            speedY += mouseSpeedY * speedMouse.y;
         }
         speedZ += Input.GetAxis("Mouse ScrollWheel") * speedMouse.z;
 
         Vector3 move = Vector3.zero;
-        move += transform.right * speedX;
+        Vector3 centralProjection = GetCentralProjection();
+        Vector3 right = -Vector3.Cross(centralProjection - transform.position, Vector3.up).normalized;
+        move += right * speedX;
+        if (speedX != 0) {
+            if (rotationCoroutine != null) {
+                StopCoroutine(rotationCoroutine);
+                rotationCoroutine = null;
+            }
+            if (lookAtCentralProjectionCoroutine == null) {
+                lookAtCentralProjectionCoroutine = LookAtCentralPoint();
+            }
+        } else {
+            if (lookAtCentralProjectionCoroutine != null) {
+                StopCoroutine(lookAtCentralProjectionCoroutine);
+                lookAtCentralProjectionCoroutine = null;
+            }
+        }
         move += Vector3.up * speedY;
         move += transform.forward * speedZ;
         move *= Time.deltaTime;
         controller.Move(move);
 
-        isMoving = move != Vector3.zero;
-        //if(rotationCoroutine != null) {
-        //    //StopCoroutine(rotationCoroutine);
-        //    //rotationCoroutine = null;
-        //}
+        isMoving = move != Vector3.zero || Input.GetMouseButton(0);
+        if(isMoving) {
+            lastIsMovingTimer.Reset();
+        }
+    }
+
+    protected void LookAtClosestInterestPoint() {
+        if (!lastIsMovingTimer.IsOver()) {
+            lastClosestLevelPosition = Vector3.one * -1;
+            if(rotationCoroutine != null) {
+                StopCoroutine(rotationCoroutine);
+                rotationCoroutine = null;
+            }
+            return;
+        }
+
+        Vector3 closest = GetClosestInterestPoint(transform.position);
+        if (rotationCoroutine != null) {
+            if (closest == lastClosestLevelPosition)
+                return;
+            else {
+                StopCoroutine(rotationCoroutine);
+                rotationCoroutine = null;
+            }
+        }
+
+        if(closest == lastClosestLevelPosition)
+            transform.LookAt(closest);
+        else
+            rotationCoroutine = ProgressivelyLookAt(closest);
+
+        lastClosestLevelPosition = closest;
+    }
+
+    protected Vector3 GetCentralProjection() {
+        Vector3 centralProjection = Vector3.Project(transform.position, Vector3.up);
+        List<Vector3> interestPoints = GetAllInterestPoints();
+        float maxY = interestPoints.Max(p => p.y);
+        float minY = interestPoints.Min(p => p.y);
+        centralProjection.y = Mathf.Clamp(centralProjection.y, minY, maxY);
+        return centralProjection;
     }
 
     protected void ApplyElasticitySphere() {
@@ -103,37 +167,22 @@ public class SelectorCameraController : MonoBehaviour {
     }
 
     protected Vector3 GetClosestInterestPoint(Vector3 pos) {
-        List<Vector3> levelsPositions = GetLevelsPositions();
-        List<Vector3> cadenasPositions = GetCadenasPositions();
-        List<Vector3> interestPoints = levelsPositions.Concat(cadenasPositions).ToList();
+        List<Vector3> interestPoints = GetAllInterestPoints();
         return interestPoints.OrderBy(p => Vector3.Distance(pos, p)).First();
     }
 
-    protected void LookAtClosestInterestPoint() {
-        Vector3 closest = GetClosestInterestPoint(transform.position);
-
-        if (rotationCoroutine != null) {
-            if (closest == lastClosestLevelPosition)
-                return;
-            else {
-                StopCoroutine(rotationCoroutine);
-                rotationCoroutine = null;
-            }
-        }
-
-        if(closest == lastClosestLevelPosition)
-            transform.LookAt(closest);
-        else
-            LookAt(closest);
-
-        lastClosestLevelPosition = closest;
+    protected List<Vector3> GetAllInterestPoints() {
+        List<Vector3> levelsPositions = GetLevelsPositions();
+        List<Vector3> cadenasPositions = GetCadenasPositions();
+        List<Vector3> interestPoints = levelsPositions.Concat(cadenasPositions).ToList();
+        return interestPoints;
     }
 
-    public void LookAt(Vector3 point) {
-        rotationCoroutine = StartCoroutine(ProgressiveLookAt(point));
+    public Coroutine ProgressivelyLookAt(Vector3 point) {
+        return StartCoroutine(CProgressiveLookAt(point));
     }
 
-    protected IEnumerator ProgressiveLookAt(Vector3 pointToLookAt) {
+    protected IEnumerator CProgressiveLookAt(Vector3 pointToLookAt) {
         Timer totalTimer = new Timer(dureeRotations);
         Timer currentTimer = new Timer(dureeRotations);
         Vector3 lastPosition = transform.position;
@@ -160,6 +209,24 @@ public class SelectorCameraController : MonoBehaviour {
         }
         transform.LookAt(pointToLookAt, Vector3.up);
         rotationCoroutine = null;
+    }
+
+    public Coroutine LookAtCentralPoint() {
+        return StartCoroutine(CLookAtCentralPoint());
+    }
+
+    protected IEnumerator CLookAtCentralPoint() {
+        while(true) {
+            Vector3 centralPoint = GetCentralProjection();
+            Vector3 directionToLookAt = centralPoint - transform.position;
+            Vector3 currentDirection = transform.forward;
+            float angle = Vector3.Angle(currentDirection, directionToLookAt);
+            Vector3 axe = Vector3.Cross(currentDirection, directionToLookAt);
+            float value = angle * Time.deltaTime * speedLookAtCenter;
+            transform.RotateAround(transform.position, axe, value);
+            transform.LookAt(transform.position + transform.forward, Vector3.up);
+            yield return null;
+        }
     }
 
     public float GetIdealDistanceFromLevel() {
