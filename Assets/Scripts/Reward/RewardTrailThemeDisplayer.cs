0ba568bdb7c2bb174c81d66fa4f7f9c6bfd7b9eb
Ekalawen <alexys.dussier@gmail.com>
2021-01-23 12:05:18 +0100

    Prevent player trail in reward from being red ! :)

--- Assets/Scripts/Reward/RewardTrailThemeDisplayer.cs
+++ Assets/Scripts/Reward/RewardTrailThemeDisplayer.cs
@@ -14,7 +14,30 @@ public class RewardTrailThemeDisplayer : RewardTrailDisplayer {
 
     public override void ResetObject() {
         base.ResetObject();
+        ResetColor();
+    }
+
+    protected void ResetColor() {
+        ReplaceSpecificColorFromThemes(ColorManager.Theme.MULTICOLOR, ColorManager.GetRandomTheme());
+        ReplaceSpecificColorFromThemes(ColorManager.Theme.MULTICOLOR_NON_BRIGHT, ColorManager.GetRandomNonBrightTheme());
+        RemoveSpecificColorFromThemes(ColorManager.Theme.ROUGE);
         trail.startColor = ColorManager.GetColor(themes);
+    }
+
+    protected void ReplaceSpecificColorFromThemes(ColorManager.Theme source, ColorManager.Theme cible) {
+        while (themes.Contains(source)) {
+            themes.Remove(source);
+            themes.Add(cible);
+        }
+    }
+
+    protected void RemoveSpecificColorFromThemes(ColorManager.Theme theme) {
+        while (themes.Contains(theme)) {
+            themes.Remove(theme);
+            if (themes.Count <= 0) {
+                themes.Add(ColorManager.GetRandomNonBrightTheme());
+            }
+        }
     }
 
     public override void Update() {
