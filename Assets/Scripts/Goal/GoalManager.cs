19bb53b0963733a78199331221c4eed11c717a92
Ekalawen <alexys.dussier@gmail.com>
2021-02-16 10:01:47 +0100

    Add GoalTresholds for each level and beginning of GoalManager :)

---
+++ Assets/Scripts/Goal/GoalManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    public List<GoalTresholds> goalTresholds;
+
+    protected GameManager gm;
+
+    public void Initialize() {
+        gm = GameManager.Instance;
+    }
+}
---
+++ Assets/Scripts/Goal/GoalTresholds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GoalTresholds", menuName = "GoalTresholds")]
+public class GoalTresholds : ScriptableObject {
+
+    public enum GoalType {
+        DATA,
+        BLOCK,
+    }
+
+    public GoalType type;
+    public List<int> tresholds;
+}
