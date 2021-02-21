4c6bd7e7ca17775582623ec31b8b09b0ef36d38c
Ekalawen <alexys.dussier@gmail.com>
2021-02-21 17:08:43 +0100

    Create the SoundBankIngame :)

---
+++ Assets/Scripts/Sound/SoundBankIngame.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SoundBankIngame", menuName = "SoundBankIngame")]
+public class SoundBankIngame : ScriptableObject {
+    public AudioClipParams jumpClips;
+    public AudioClipParams landClips;
+    public AudioClipParams gripClips;
+    public AudioClipParams createCubeClips;
+    public AudioClipParams getLumiereClips;
+    public AudioClipParams getItemClips;
+    public AudioClipParams failActionClips;
+    public AudioClipParams hitClips;
+    public AudioClipParams hitTracerClips;
+    public AudioClipParams emissionTracerClips;
+    public AudioClipParams firstBossPresenceClips;
+    public AudioClipParams firstBossDecreasingBall;
+    public AudioClipParams firstBossIncreasingBall;
+    public AudioClipParams detectionClips;
+    public AudioClipParams dissimuleClips;
+    public AudioClipParams timeOutClips;
+    public AudioClipParams receivedMessageClips;
+    public AudioClipParams newBlockClips;
+    public AudioClipParams eventStartClips;
+    public AudioClipParams eventEndClips;
+    public AudioClipParams jumpEventStartClips;
+    public AudioClipParams jumpSuccessClips;
+    public AudioClipParams jumpEventStunClips;
+    public AudioClipParams jumpEventUnStunClips;
+    public AudioClipParams intersectionEventClips;
+    public AudioClipParams activationPouvoirClips;
+    public AudioClipParams deniedPouvoirClips;
+    public AudioClipParams notFoundPouvoirClips;
+    public AudioClipParams timeZoneButtonInClips;
+    public AudioClipParams timeZoneButtonOutClips;
+    public AudioClipParams bounceClips;
+    public AudioClipParams rewardBestScoreClips;
+    public AudioClipParams victoryClips;
+    public AudioClipParams defeatClips;
+
+    public AudioClipParams normalMusics;
+    public AudioClipParams endGameMusics;
+}
