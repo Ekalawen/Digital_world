fa31b85bc9696e187f47dc0583a24bea18d06b9a
Ekalawen <alexys.dussier@gmail.com>
2021-03-10 17:29:37 +0100

    Rework of the ResetTimeItem particles ! :)

---
+++ Assets/Scripts/Map/MapElements/MapElementsComponents/AutoLookAtPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoLookAtPlayer : MonoBehaviour {
+
+    protected Transform playerTransform;
+
+    void Start() {
+        playerTransform = GameManager.Instance.player.transform;
+    }
+
+    void Update() {
+        transform.LookAt(playerTransform);
+    }
+}
