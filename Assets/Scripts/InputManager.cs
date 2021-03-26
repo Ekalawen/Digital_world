21ece69f774749c48275638ba4d630fcf05104c7
Ekalawen <alexys.dussier@gmail.com>
2021-03-26 13:25:56 +0100

    Add CONTROLLER to KeybindingType enum

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,8 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour {
+
+    public enum KeybindingType {
+        AZERTY,
+        QWERTY,
+        CONTROLLER,
+    };
+
     static InputManager _instance;
     public static InputManager Instance { get { return _instance ?? (_instance = new GameObject().AddComponent<InputManager>()); } }
+
+    public KeybindingType currentKeybindingType;
 
     void Awake() {
         if (!_instance) { _instance = this; }
@@ -15,6 +24,33 @@ public class InputManager : MonoBehaviour {
     public void Initialize() {
         name = "InputManager";
         DontDestroyOnLoad(this);
+        currentKeybindingType = GetKeybindingType();
+    }
+
+    protected int GetKeybindingTypeIndice() {
+        return PrefsManager.GetInt(PrefsManager.KEYBINDING_INDICE_KEY, 0);
+    }
+
+    protected KeybindingType GetKeybindingType() {
+        return (KeybindingType)GetKeybindingTypeIndice();
+    }
+
+    public KeybindingType GetCurrentKeybindingType() {
+        return currentKeybindingType;
+    }
+
+    public int GetCurrentKeybindingTypeIndice() {
+        return (int)currentKeybindingType;
+    }
+
+    public void SetKeybindingType(KeybindingType keybindingType) {
+        int index = (int)keybindingType;
+        SetKeybindingTypeByIndice(index);
+    }
+
+    public void SetKeybindingTypeByIndice(int keybindingIndice) {
+        PrefsManager.SetInt(PrefsManager.KEYBINDING_INDICE_KEY, keybindingIndice);
+        currentKeybindingType = (KeybindingType)keybindingIndice;
     }
 
     public Vector2 GetMouseMouvement() {
@@ -49,7 +85,7 @@ public class InputManager : MonoBehaviour {
     }
 
     public KeyCode GetPouvoirAKeyCode() {
-        if (KeybindingDropdown.GetKeybinding() == KeybindingDropdown.KeybindingType.AZERTY) {
+        if (GetCurrentKeybindingType() == KeybindingType.AZERTY) {
             return KeyCode.A;
         } else {
             return KeyCode.Q;
@@ -81,10 +117,15 @@ public class InputManager : MonoBehaviour {
     }
 
     public Vector3 GetHorizontalMouvement() {
-        if (KeybindingDropdown.GetKeybinding() == KeybindingDropdown.KeybindingType.AZERTY) {
-            return new Vector3(Input.GetAxis("HorizontalAZERTY"), 0, Input.GetAxis("VerticalAZERTY"));
-        } else {
-            return new Vector3(Input.GetAxis("HorizontalQWERTY"), 0, Input.GetAxis("VerticalQWERTY"));
+        switch (GetCurrentKeybindingType()) {
+            case KeybindingType.AZERTY:
+                return new Vector3(Input.GetAxis("HorizontalAZERTY"), 0, Input.GetAxis("VerticalAZERTY"));
+            case KeybindingType.QWERTY:
+                return new Vector3(Input.GetAxis("HorizontalQWERTY"), 0, Input.GetAxis("VerticalQWERTY"));
+            case KeybindingType.CONTROLLER:
+                return Vector3.zero;
+            default:
+                return Vector3.zero;
         }
     }
 }
--- Assets/Scripts/UI/Menu/Utilitaires/KeybindingDropdown.cs
+++ Assets/Scripts/UI/Menu/Utilitaires/KeybindingDropdown.cs
@@ -6,32 +6,21 @@ using UnityEngine.UI;
 
 public class KeybindingDropdown : MonoBehaviour {
 
-    public enum KeybindingType {
-        AZERTY,
-        QWERTY,
-    };
-
     public Dropdown dropdown;
 
     public void Start() {
         List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
-        options.Add(new Dropdown.OptionData(KeybindingType.AZERTY.ToString()));
-        options.Add(new Dropdown.OptionData(KeybindingType.QWERTY.ToString()));
+        options.Add(new Dropdown.OptionData(InputManager.KeybindingType.AZERTY.ToString()));
+        options.Add(new Dropdown.OptionData(InputManager.KeybindingType.QWERTY.ToString()));
+        options.Add(new Dropdown.OptionData(InputManager.KeybindingType.CONTROLLER.ToString()));
         dropdown.options = options;
 
-        dropdown.value = GetKeybindingIndice();
+        dropdown.value = InputManager.Instance.GetCurrentKeybindingTypeIndice();
         dropdown.onValueChanged.AddListener(LocaleSelected);
     }
 
     static void LocaleSelected(int index) {
-        PrefsManager.SetInt(PrefsManager.KEYBINDING_INDICE_KEY, index);
+        InputManager.Instance.SetKeybindingTypeByIndice(index);
     }
 
-    public static int GetKeybindingIndice() {
-        return PrefsManager.GetInt(PrefsManager.KEYBINDING_INDICE_KEY, 0);
-    }
-
-    public static KeybindingType GetKeybinding() {
-        return (KeybindingType)GetKeybindingIndice();
-    }
 }
