6aa94dd3f95bca8487efc141f1e4278eba5a543a
Ekalawen <alexys.dussier@gmail.com>
2021-03-26 21:38:04 +0100

    Display Manette XBOX instead of Controller :)

--- Assets/Scripts/UI/Menu/Utilitaires/KeybindingDropdown.cs
+++ Assets/Scripts/UI/Menu/Utilitaires/KeybindingDropdown.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
 using UnityEngine.UI;
 
 public class KeybindingDropdown : MonoBehaviour {
 
     public Dropdown dropdown;
+    public LocalizedString controllerString;
 
     public void Start() {
         List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
         options.Add(new Dropdown.OptionData(InputManager.KeybindingType.AZERTY.ToString()));
         options.Add(new Dropdown.OptionData(InputManager.KeybindingType.QWERTY.ToString()));
-        options.Add(new Dropdown.OptionData(InputManager.KeybindingType.CONTROLLER.ToString()));
+        options.Add(new Dropdown.OptionData(controllerString.GetLocalizedString().Result));
         dropdown.options = options;
 
         dropdown.value = InputManager.Instance.GetCurrentKeybindingTypeIndice();
