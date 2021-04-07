860878b5b76f1c6729e90aa91d2a4ea5cd048026
Ekalawen <alexys.dussier@gmail.com>
2021-04-07 13:51:15 +0200

    Only remove gravity on dash when we are targeting down with 15 degree treshold ! :)

--- Assets/Scripts/Pouvoirs/PouvoirDash.cs
+++ Assets/Scripts/Pouvoirs/PouvoirDash.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,23 @@ public class PouvoirDash : IPouvoir {
 
     public float distance = 3.0f;
     public float duree = 0.15f;
+    public float removeGravityTresholdAngle = 15.0f;
 
     protected override bool UsePouvoir() {
         Vector3 direction = player.camera.transform.forward;
         Poussee poussee = new Poussee(direction, duree, distance);
         player.AddPoussee(poussee);
         player.ResetGrip();
-        player.RemoveGravityEffectFor(duree);
+        RemoveGravityEffect(direction);
         gm.postProcessManager.StartDashVfx(duree);
         return true;
+    }
+
+    protected void RemoveGravityEffect(Vector3 direction) {
+        float angle = Mathf.Abs(Vector3.Angle(direction, gm.gravityManager.Down()));
+        if (90 - angle <= removeGravityTresholdAngle) {
+            Debug.Log($"angle = {angle}");
+            player.RemoveGravityEffectFor(duree);
+        }
     }
 }
