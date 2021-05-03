cd702ffe9a8fee8c96e1b20b3a669cc071a01f35
Ekalawen <alexys.dussier@gmail.com>
2021-05-03 19:49:25 +0200

    Lightning always face camera ! :)

---
+++ Assets/Scripts/Map/MapElements/MapElementsComponents/AutoOrientLightning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoOrientLightning : MonoBehaviour {
+
+    protected Vector3 startingAxe;
+    protected Transform playerTransform;
+    protected GameManager gm;
+
+    void Start() {
+        gm = GameManager.Instance;
+        startingAxe = transform.forward;
+    }
+
+    void Update() {
+        //Vector3 cameraProjectedPos = Vector3.ProjectOnPlane(cameraTransform.position, startingAxe);
+        Vector3 playerDirection = gm.player.transform.position - transform.position;
+        transform.LookAt(transform.position + startingAxe, playerDirection);
+    }
+}
