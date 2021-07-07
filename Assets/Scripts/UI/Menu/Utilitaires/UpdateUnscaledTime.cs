44094012dffd92b4079bd51c56ca69f2c71b8428
Ekalawen <alexys.dussier@gmail.com>
2021-07-07 23:50:32 +0200

    Make buttons and sliders use UpdateUnscaleTime to function in game pause :)

---
+++ Assets/Scripts/UI/Menu/Utilitaires/UpdateUnscaledTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUnscaledTime : MonoBehaviour {
+
+    protected Material material;
+
+    public void Start() {
+        material = gameObject.GetComponent<Image>().material;
+    }
+
+    public void Update() {
+        material.SetFloat("_UnscaledTime", Time.unscaledTime);
+    }
+}
