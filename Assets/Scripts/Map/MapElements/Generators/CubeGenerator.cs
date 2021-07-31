f9c5601ee1131cdda41e708b2253ff7340f04fb8
Ekalawen <alexys.dussier@gmail.com>
2021-07-31 19:05:21 +0200

    Make CubeGenerators chose it's cubes better and add dissolveTime to it + add Textures to Generators ! :)

--- Assets/Scripts/Map/MapElements/Generators/CubeGenerator.cs
+++ Assets/Scripts/Map/MapElements/Generators/CubeGenerator.cs
@@ -9,29 +9,33 @@ public class CubeGenerator : IGenerator {
     [Header("CubeGenerator")]
     public Cube.CubeType cubeType = Cube.CubeType.NORMAL;
     public bool setLinky = false;
+    public float dissolveEffectDuration = 0.1f;
 
     protected override void GenerateOneSpecific(Vector3 position) {
         Cube cube = map.GetCubeAt(position);
         if(choseType == ChoseType.GET_CUBES) {
             if(cube != null) {
                 Cube newCube = map.SwapCubeType(cube, cubeType);
-                if(newCube != null && setLinky) {
-                    newCube.SetLinkyValue(true);
-                }
+                SetupNewCube(newCube);
             }
         } else {
             if(cube == null) {
                 Cube newCube = map.AddCube(position, cubeType);
-                if(newCube != null && setLinky) {
-                    newCube.SetLinkyValue(true);
-                }
+                SetupNewCube(newCube);
             }
+        }
+    }
+
+    protected void SetupNewCube(Cube newCube) {
+        newCube.StartDissolveEffect(dissolveEffectDuration);
+        if(newCube != null && setLinky) {
+            newCube.SetLinkyValue(true);
         }
     }
 
     protected override bool IsValidPosition(Vector3 position) {
         Cube cube = map.GetCubeAt(position);
-        return (choseType == ChoseType.GET_CUBES && cube != null)
+        return (choseType == ChoseType.GET_CUBES && cube != null && cube.type != cubeType)
             || (choseType == ChoseType.GET_EMPTY && cube == null);
     }
 }
