881c4a11138e944226fe155666c59fe5eba3b949
Ekalawen <alexys.dussier@gmail.com>
2021-08-01 12:57:55 +0200

    Create EnnemiGenerator and EnnemiGeneratorFirstBossSondes ! :)

--- Assets/Scripts/Map/MapElements/Generators/EnnemiGenerator.cs
+++ Assets/Scripts/Map/MapElements/Generators/EnnemiGenerator.cs
@@ -10,24 +10,39 @@ public class EnnemiGenerator : IGenerator {
     public GameObject ennemiPrefab;
     public int nbEnnemisMax = 10;
     public bool destroyEnnemisOnDestruction = true;
+    public GameObject ennemiLightningLinkPrefab;
 
     protected List<Ennemi> ennemisCreated = new List<Ennemi>();
 
     protected override void GenerateOneSpecific(Vector3 position) {
         if (CanAddNewEnnemi()) {
             Cube cube = map.GetCubeAt(position);
-            if (cube != null)
-            {
+            if (cube != null) {
                 cube.Explode();
             }
             Ennemi ennemi = gm.ennemiManager.GenerateEnnemiFromPrefab(ennemiPrefab, position);
-            ennemisCreated.Add(ennemi);
+            SetupEnnemi(ennemi);
         }
+    }
+
+    protected void SetupEnnemi(Ennemi ennemi) {
+        ennemisCreated.Add(ennemi);
+        StartCoroutine(CLinkEnnemiToGenerator(ennemi));
     }
 
     protected bool CanAddNewEnnemi() {
         ennemisCreated = ennemisCreated.FindAll(e => e != null);
         return ennemisCreated.Count < nbEnnemisMax;
+    }
+
+    protected IEnumerator CLinkEnnemiToGenerator(Ennemi ennemi) {
+        Lightning link = Instantiate(ennemiLightningLinkPrefab).GetComponent<Lightning>();
+        link.Initialize(transform.position, ennemi.transform.position);
+        while (ennemi != null) {
+            link.SetPosition(transform.position, ennemi.transform.position);
+            yield return null;
+        }
+        Destroy(link.gameObject);
     }
 
     protected override bool IsValidPosition(Vector3 position) {
