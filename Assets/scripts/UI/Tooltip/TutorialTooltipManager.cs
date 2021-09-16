e970f67b184f946122701734ba48e3d860dc6f4d
Ekalawen <alexys.dussier@gmail.com>
2021-09-16 13:54:58 +0200

    Add TutorialTooltip to read the doc ! :)

--- Assets/scripts/UI/Tooltip/TutorialTooltipManager.cs
+++ Assets/scripts/UI/Tooltip/TutorialTooltipManager.cs
@@ -11,29 +11,41 @@ using UnityEngine.UI;
 public class TutorialTooltipManager : MonoBehaviour {
 
     [Header("TutorialTooltips")]
+    [Header("Mouvement")]
     public GameObject tutorialTooltipSelectorMouvement;
     public RectTransform tutorialTooltipSelectorMouvementTransform;
 
+    [Header("Select Level")]
     public GameObject tutorialTooltipSelectorLevel;
     public RectTransform tutorialTooltipSelectorLevelTransform;
 
+    [Header("Select Path")]
     public GameObject tutorialTooltipSelectorCadenas;
     public RectTransform tutorialTooltipSelectorCadenasTransform;
 
+    [Header("ToDH")]
     public GameObject tutorialTooltipToDH;
     public RectTransform tutorialTooltipToDHTransform;
 
+    [Header("Open DH")]
     public GameObject tutorialTooltipOpenDH;
     public RectTransform tutorialTooltipOpenDHTransform;
 
+    [Header("Hacker")]
     public GameObject tutorialTooltipHacker;
     public RectTransform tutorialTooltipHackerTransform;
 
+    [Header("To Next Level")]
     public GameObject tutorialTooltipToNextLevel;
     public RectTransform tutorialTooltipToNextLevelTransform;
 
+    [Header("Redo Previous Level")]
     public GameObject tutorialTooltipRedoPreviousLevel;
     public RectTransform tutorialTooltipRedoPreviousLevelTransform;
+
+    [Header("Read Doc")]
+    public GameObject tutorialTooltipReadDoc;
+    public RectTransform tutorialTooltipReadDocTransform;
 
     protected SelectorManager selectorManager;
     protected List<GameObject> prefabsAlreadyInstantiated;
@@ -64,6 +76,11 @@ public class TutorialTooltipManager : MonoBehaviour {
         if (selectorManager.GetLevelIndice(level) == 0) {
             if (level.IsSucceeded() && !PrefsManager.GetBool(GetKey(tutorialTooltipToDH), false)) {
                 InstantiateTutorialTooltip(tutorialTooltipToDH, tutorialTooltipToDHTransform, parent: level.menuLevel.transform);
+            }
+        }
+        if(level.GetNameId() == "FirstSondeScene") {
+            if(!PrefsManager.GetBool(GetKey(tutorialTooltipReadDoc), false)) {
+                InstantiateTutorialTooltip(tutorialTooltipReadDoc, tutorialTooltipReadDocTransform, parent: level.menuLevel.transform);
             }
         }
     }
