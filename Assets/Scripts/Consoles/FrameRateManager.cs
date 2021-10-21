7b7265fc3a5edb84d12436789bba9ab91b56e871
Ekalawen <alexys.dussier@gmail.com>
2021-10-21 22:07:35 +0200

    Compute mean SpeedCoefMultiplier of the player :)

--- Assets/Scripts/Consoles/FrameRateManager.cs
+++ Assets/Scripts/Consoles/FrameRateManager.cs
@@ -17,21 +17,27 @@ public class FrameRateManager : MonoBehaviour {
     protected List<float> deltaTimeList;
     protected Timer frameRateTimer;
 
+    protected List<float> speedCoefs;
+
     public void Initialize() {
         deltaTimeList = new List<float>();
+        speedCoefs = new List<float>();
         frameRateTimer = new Timer(0.1f, setOver: true);
         showFrameRate = PrefsManager.GetBool(PrefsManager.FPS_COUNTER_KEY, MenuOptions.defaultFpsCounter);
         SetVisibility(showFrameRate);
     }
 
     public void Tick() {
+        //PrintMeanSpeed();
         if (!showFrameRate)
             return;
-        if (frameRateTimer.IsOver()) {
+        if (frameRateTimer.IsOver())
+        {
             frameRateTimer.Reset();
             float deltaTime = Time.deltaTime;
             deltaTimeList.Add(deltaTime);
-            if (deltaTimeList.Count > 10) {
+            if (deltaTimeList.Count > 10)
+            {
                 deltaTimeList.RemoveAt(0);
             }
             float meanDeltaTime = deltaTimeList.Average();
@@ -39,6 +45,12 @@ public class FrameRateManager : MonoBehaviour {
             int ms = Mathf.RoundToInt(meanDeltaTime * 1000.0f);
             frameRateText.text = $"FPS : {fps} ({ms}ms)";
         }
+    }
+
+    protected void PrintMeanSpeed() {
+        speedCoefs.Add(GameManager.Instance.player.GetSpeedMultiplier());
+        float meanCoef = speedCoefs.Average();
+        Debug.Log($"Vitesse moyenne = {meanCoef}");
     }
 
     public void SetToAccordingConsolePosition(bool isConsoleDisplayed) {
