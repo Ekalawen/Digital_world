d837090ff8c5c8fc81fddbef5ab244ee9c35470e
Ekalawen <alexys.dussier@gmail.com>
2021-11-04 19:35:51 +0100

    Add transitions and hide GeoSphere ! :)

--- Assets/Scripts/Scans/GeoSphere/GeoSphere.cs
+++ Assets/Scripts/Scans/GeoSphere/GeoSphere.cs
@@ -2,17 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class GeoSphere : MonoBehaviour {
 
+    public static string VFX_BASE_STATE = "_BaseState";
+    public static string VFX_TRANSITION_IN_STARTING_TIME = "_TransitionINStartingTime";
+    public static string VFX_TRANSITION_IN_DURATION = "_TransitionINDuration";
+    public static string VFX_TRANSITION_OUT_STARTING_TIME = "_TransitionOUTStartingTime";
+    public static string VFX_TRANSITION_OUT_DURATION = "_TransitionOUTDuration";
+    
+    public enum SphereTransitionType { IN, OUT };
+
+    [Header("Links")]
     public GameObject geoPointPrefab;
+
+    [Header("Material")]
+    public Renderer renderer;
+    public float transitionInDuration = 3.0f;
+    public float transitionOutDuration = 1.0f;
 
     protected GameManager gm;
     protected List<GeoPoint> geoPoints;
+    protected Material material;
 
     public void Initialize() {
         gm = GameManager.Instance;
         geoPoints = new List<GeoPoint>();
+        renderer.material = new Material(renderer.material);
+        material = renderer.material;
+        HideSphere();
+    }
+
+    protected void HideSphere() {
+        material.SetFloat(VFX_BASE_STATE, 0);
+        material.SetFloat(VFX_TRANSITION_IN_STARTING_TIME, Time.time - transitionInDuration);
+        material.SetFloat(VFX_TRANSITION_IN_DURATION, transitionInDuration);
+        material.SetFloat(VFX_TRANSITION_OUT_STARTING_TIME, Time.time - transitionOutDuration);
+        material.SetFloat(VFX_TRANSITION_OUT_DURATION, transitionOutDuration);
+    }
+
+    protected void StartSphereTransition(SphereTransitionType type) {
+        if (type == SphereTransitionType.IN) {
+            material.SetFloat(VFX_BASE_STATE, 1);
+            material.SetFloat(VFX_TRANSITION_IN_STARTING_TIME, Time.time);
+            material.SetFloat(VFX_TRANSITION_IN_DURATION, transitionInDuration);
+        } else {
+            material.SetFloat(VFX_BASE_STATE, 0);
+            material.SetFloat(VFX_TRANSITION_OUT_STARTING_TIME, Time.time);
+            material.SetFloat(VFX_TRANSITION_OUT_DURATION, transitionOutDuration);
+        }
     }
 
     public void Update() {
@@ -26,6 +65,9 @@ public class GeoSphere : MonoBehaviour {
     public GeoPoint AddGeoPoint(GeoData geoData) {
         GeoPoint geoPoint = Instantiate(geoPointPrefab, parent: transform).GetComponent<GeoPoint>();
         geoPoint.Initialize(this, geoData);
+        if(geoPoints.Count == 0) {
+            StartSphereTransition(SphereTransitionType.IN);
+        }
         geoPoints.Add(geoPoint);
         return geoPoint;
     }
@@ -42,7 +84,11 @@ public class GeoSphere : MonoBehaviour {
     }
 
     public bool RemoveGeoPoint(GeoPoint geoPoint) {
-        return geoPoints.Remove(geoPoint);
+        bool succeeded = geoPoints.Remove(geoPoint);
+        if(geoPoints.Count == 0) {
+            StartSphereTransition(SphereTransitionType.OUT);
+        }
+        return succeeded;
     }
 
     public Vector3 GetPlayerPosition() {
