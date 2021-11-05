7603a982f0580a7ac1c127e63e18e4a3411dca1d
Ekalawen <alexys.dussier@gmail.com>
2021-11-05 23:09:20 +0100

    Add Helper for SoulRobberTutorial and touch the SR ! :)

---
+++ Assets/Scripts/Events/Helpers/HelperSoulRobberTutorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HelperSoulRobberTutorial : MonoBehaviour {
+
+    public HelperMessage letSoulRobberTouchYou;
+
+    protected GameManager gm;
+    protected bool hasLetSoulRobberTouchYou = false;
+
+    public void Start() {
+        gm = GameManager.Instance;
+        SoulRobber soulRobber = gm.ennemiManager.GetEnnemisOfType<SoulRobber>().First();
+        SoulRobberController soulRobberController = soulRobber.GetComponent<SoulRobberController>();
+        soulRobberController.startEscapingEvents.AddListener(new UnityAction(RegisterSoulRobberHadTouchYou));
+    }
+
+    public void Update() {
+        if (!hasLetSoulRobberTouchYou && gm.timerManager.GetElapsedTime() >= letSoulRobberTouchYou.GetTiming()) {
+            letSoulRobberTouchYou.DisplayMessage();
+        }
+    }
+
+    public void RegisterSoulRobberHadTouchYou() {
+        hasLetSoulRobberTouchYou = true;
+    }
+}
