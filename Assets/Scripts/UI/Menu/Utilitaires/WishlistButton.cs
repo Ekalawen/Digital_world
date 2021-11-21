153acd797c39818b713fafe4167826a38323d1f6
Ekalawen <alexys.dussier@gmail.com>
2021-11-21 22:03:47 +0100

    Add WishlistButton in selector ! :)

---
+++ Assets/Scripts/UI/Menu/Utilitaires/WishlistButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WishlistButton : MonoBehaviour {
+    public void Start() {
+        SelectorManager selectorManager = SelectorManager.Instance;
+        gameObject.SetActive(selectorManager.isDemo);
+    }
+}
