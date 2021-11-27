8e8bf0c94fed40bee4a8e07c45f13d9ecb612f8b
Ekalawen <alexys.dussier@gmail.com>
2021-11-27 17:23:19 +0100

    Increase title sizes in Selector based on the distance of the Camera + auto-adjust fontSize of titles :)

--- Assets/Scripts/UI/Selector/SelectorLevel/SelectorLevelObjectTitle.cs
+++ Assets/Scripts/UI/Selector/SelectorLevel/SelectorLevelObjectTitle.cs
@@ -8,15 +8,41 @@ public class SelectorLevelObjectTitle : MonoBehaviour {
 
     public SelectorLevelObject objectLevel;
 
+    public Vector2 titleSizeRange = new Vector2(1, 1.5f);
+    public Vector2 canvasHeightRange = new Vector2(1, 1.6f);
+    public Vector2 titleDistanceRange = new Vector2(10, 20);
+    public float disableTreshold = 50.0f;
     public TMPro.TMP_Text text;
     public Image background;
     public Material materialUnlocked;
     public Material materialLocked;
     public Material materialFocused;
+    public Canvas canvas;
 
     public void Initialize() {
         SetTitleToLevelName();
         SetUnfocused();
+    }
+
+    public void Update() {
+        UpdateBackgroundScaleAndEnable();
+    }
+
+    protected void UpdateBackgroundScaleAndEnable() {
+        //float cameraDistance = Vector3.Distance(SelectorManager.Instance.baseCamera.transform.position, transform.position);
+        Vector3 cameraPosition = SelectorManager.Instance.baseCamera.transform.position;
+        float cameraDistanceToCenter = Vector3.Distance(Vector3.ProjectOnPlane(cameraPosition, Vector3.up), Vector3.zero);
+        Debug.Log($"CameraDistanceToCenter = {cameraDistanceToCenter}");
+        if(cameraDistanceToCenter > disableTreshold) {
+            background.gameObject.SetActive(false);
+        } else {
+            background.gameObject.SetActive(true);
+            float avancement = MathCurves.LinearReversed(titleDistanceRange.x, titleDistanceRange.y, cameraDistanceToCenter);
+            float scale = MathCurves.Linear(titleSizeRange.x, titleSizeRange.y, avancement);
+            background.transform.localScale = Vector3.one * scale;
+            float canvasHeight = MathCurves.Linear(canvasHeightRange.x, canvasHeightRange.y, avancement);
+            canvas.transform.localPosition = new Vector3(0, canvasHeight, 0);
+        }
     }
 
     protected void SetTitleToLevelName() {
