88221762af8bdde41863f94069d301d1c0188a0f
Ekalawen <alexys.dussier@gmail.com>
2022-02-17 17:41:04 +0100

    Add Achievement GetDataCount ! :)

---
+++ Assets/Scripts/Steam/Achievements/Regular/Achievement_GetDataCount.cs
@@ -0,0 +1,22 @@
+using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Localization;
+
+public class Achievement_GetDataCount : Achievement {
+
+    [Header("Parameters")]
+    public int treshold = 100;
+
+    protected override void InitializeSpecific() {
+        gm.eventManager.onCaptureLumiere.AddListener(UnlockIfBetterTreshold);
+    }
+
+    public void UnlockIfBetterTreshold() {
+        if(Lumiere.GetCurrentDataCount() >= treshold) {
+            Unlock();
+        }
+    }
+}
