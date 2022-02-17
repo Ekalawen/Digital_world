7f65fc4a3658dc1ba08ba669a6f0c9fbecc76291
Ekalawen <alexys.dussier@gmail.com>
2022-02-17 15:18:11 +0100

    Add Achievements UnlockPath based on number of tries ! :)

---
+++ Assets/Scripts/Steam/Achievements/Selector/Achievement_UnlockPath_WithNumberOfTries.cs
@@ -0,0 +1,21 @@
+using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.SceneManagement;
+
+public class Achievement_UnlockPath_WithNumberOfTries : Achievement_UnlockPath {
+
+    public int nbTriesTreshold = 10;
+    public bool exactlyThisTreshold = false;
+
+    protected override void UnlockPath(SelectorPath selectorPath) {
+        string key = selectorPath.GetNameId() + PrefsManager.NB_SUBMITS_PATH_KEY;
+        int nbTries = PrefsManager.GetInt(key, 0);
+        if(exactlyThisTreshold ? nbTries == nbTriesTreshold - 1 : nbTries >= nbTriesTreshold - 1) {
+            Unlock();
+        }
+    }
+}
