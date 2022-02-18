231bec898fac75b401ffd795a64efb6e57182ba4
Ekalawen <alexys.dussier@gmail.com>
2022-02-18 20:21:58 +0100

    Move and rename Achievement_LessNbUsePouvoirsSpecific into AchievementSpecific_LessNbUsePouvoirs :)

--- Assets/scripts/Steam/Achievements/Specific/AchievementSpecific_LessNbUsePouvoirs.cs
+++ Assets/scripts/Steam/Achievements/Specific/AchievementSpecific_LessNbUsePouvoirs.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Localization;
 
-public class Achievement_LessNbUsePouvoirsSpecific : Achievement_FinishLevel {
+public class AchievementSpecific_LessNbUsePouvoirs : Achievement_FinishLevel {
 
     public PouvoirDisplay.PouvoirType pouvoirType = PouvoirDisplay.PouvoirType.DASH;
     public int treshold = 1;
