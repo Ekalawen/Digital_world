e52c7b5ff68b8ec9a47ab2fadcdeea08c67b5428
Ekalawen <alexys.dussier@gmail.com>
2022-03-15 11:55:49 +0100

    Add DH to level TargetedCorruption ! :)

---
+++ Assets/Scripts/UI/Selector/SelectorPath/CustomPasses/CustomPasseNbDatas.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine.Localization;
+
+public class CustomPasseNbDatas : CustomPasse {
+    public override string GetPasse(SelectorPath selectorPath) {
+        int nbDatas = PrefsManager.GetInt(PrefsManager.TOTAL_DATA_COUNT_KEY, 0);
+        return nbDatas.ToString();
+    }
+}
