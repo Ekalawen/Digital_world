a23f35934b42115c57fc24edb936655f6e13718f
Ekalawen <alexys.dussier@gmail.com>
2022-04-08 14:53:34 +0200

    Create EndScreens for trailer :)

--- Assets/Scripts/Map/MapElements/MapElementsComponents/AutoActivator.cs
+++ Assets/Scripts/Map/MapElements/MapElementsComponents/AutoActivator.cs
@@ -10,6 +10,7 @@ public class AutoActivator : MonoBehaviour {
     public GameObject go; // It's not really an "auto" as an object can re-activate himself ! :)
     public float activateIn = 3.0f;
     public float loopDuration = 7.0f;
+    public float inDuration = 0.3f;
     public List<MonoBehaviour> componentsToActivate;
 
     public void Start() {
@@ -26,7 +27,19 @@ public class AutoActivator : MonoBehaviour {
         foreach(MonoBehaviour component in componentsToActivate) {
             component.enabled = true;
         }
-        yield return new WaitForSeconds(loopDuration - activateIn);
+        yield return GrowIn(inDuration);
+        yield return new WaitForSeconds(loopDuration - activateIn - inDuration);
         StartCoroutine(Loop());
+    }
+
+    protected IEnumerator GrowIn(float duration) {
+        Timer timer = new Timer(duration);
+        float initialScale = go.transform.localScale.x;
+        go.transform.localScale = Vector3.zero;
+        while(!timer.IsOver()) {
+            go.transform.localScale = Vector3.one * timer.GetAvancement() * initialScale;
+            yield return null;
+        }
+        go.transform.localScale = Vector3.one * initialScale;
     }
 }
