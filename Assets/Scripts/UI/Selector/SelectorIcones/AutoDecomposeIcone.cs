8afb5f7cd94e28b1fd42a87a8d34b66a5c22fbe6
Ekalawen <alexys.dussier@gmail.com>
2022-04-21 13:10:19 +0200

    Add Icone for MultiRunnerDemo :)

--- Assets/Scripts/UI/Selector/SelectorIcones/AutoDecomposeIcone.cs
+++ Assets/Scripts/UI/Selector/SelectorIcones/AutoDecomposeIcone.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class AutoDecomposeIcone : MonoBehaviour {
 
     public float dureeDecompose = 2.0f;
+    public float dureeAfterDecompose = 0;
+    public float dureeBeforeDecompose = 0;
 
     protected Material material;
 
@@ -16,13 +18,19 @@ public class AutoDecomposeIcone : MonoBehaviour {
 
     protected IEnumerator CDecomposePeriodically() {
         while (true) {
+            SetNotDecomposed();
+            yield return new WaitForSeconds(dureeBeforeDecompose);
             Decompose();
-            yield return new WaitForSeconds(dureeDecompose);
+            yield return new WaitForSeconds(dureeDecompose + dureeAfterDecompose);
         }
     }
 
+    protected void SetNotDecomposed() {
+        material.SetFloat("_DecomposeTime", dureeBeforeDecompose);
+        material.SetFloat("_DecomposeStartingTime", Time.time + dureeBeforeDecompose);
+    }
 
-    public void Decompose() {
+    protected void Decompose() {
         material.SetFloat("_DecomposeTime", dureeDecompose);
         material.SetFloat("_DecomposeStartingTime", Time.time);
     }
