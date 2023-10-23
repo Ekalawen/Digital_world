2229ae49a5d793341bfc3ee86661698889b7a532
My Name <mail@gmail.com>
2023-10-23 22:21:29 +0200

    Create OverrideProgressiveExplosions ! <3

---
+++ Assets/Scripts/Map/Overrides/OverrideProgressiveExplosions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class OverrideProgressiveExplosions : Override {
+
+    public float probability = 0.2f;
+    public float range = 5.0f;
+
+    protected Player player;
+    protected List<Cube> safeCubes;
+
+    protected override void InitializeSpecific() {
+        player = gm.player;
+        safeCubes = new List<Cube>();
+    }
+
+    protected void Update() {
+        List<Cube> cubesInRange = map.GetCubesInSphere(player.transform.position, range);
+        cubesInRange = cubesInRange.FindAll(c => !safeCubes.Contains(c));
+        foreach (Cube cube in cubesInRange) {
+            if (UnityEngine.Random.value < probability) {
+                cube.Explode();
+            } else {
+                safeCubes.Add(cube);
+            }
+        }
+    }
+}
