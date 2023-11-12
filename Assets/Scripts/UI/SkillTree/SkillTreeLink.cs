286dd4864dcbe6ba334dc73cf656b138ed622697
Ekalawen <>
2023-11-12 20:43:06 +0100

    Change colors of the SkillTreeLines :)

--- Assets/Scripts/UI/SkillTree/SkillTreeLink.cs
+++ Assets/Scripts/UI/SkillTree/SkillTreeLink.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class SkillTreeLink : MonoBehaviour {
 
     public UILineRenderer lineRenderer;
+    public GameObject lineUnlockedPrefab;
+    public GameObject lineLockedPrefab;
 
     protected SkillTreeManager skillTreeManager;
     protected SkillTreeUpgrade sourceUpgrade;
@@ -23,6 +25,7 @@ public class SkillTreeLink : MonoBehaviour {
         Vector2 source = sourceUpgrade.GetComponent<RectTransform>().anchoredPosition;
         Vector2 target = targetUpgrade.GetComponent<RectTransform>().anchoredPosition;
         List<Vector2> positions = new List<Vector2>() { source, target };
+        lineRenderer.linePrefab = UnityEngine.Random.value < 0.5f ? lineUnlockedPrefab : lineLockedPrefab;
         lineRenderer.Initialize(positions);
     }
 }
