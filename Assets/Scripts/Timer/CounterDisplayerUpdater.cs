3bf723f9238ae1efd514dd4eada3d7a056a56d5a
Ekalawen <>
2024-01-14 14:14:25 +0100

    Create Bounce on CounterDisplayerUpdater when value change ! :)

--- Assets/Scripts/Timer/CounterDisplayerUpdater.cs
+++ Assets/Scripts/Timer/CounterDisplayerUpdater.cs
@@ -12,22 +12,28 @@ public class CounterDisplayerUpdater : MonoBehaviour {
     public string prefix = "";
     public string suffix = "";
     public float updateDuration = 0.6f;
+    public float bounceScale = 2.0f;
+    public AnimationCurve bounceCurve;
     public bool useCreditsFormating = false;
 
     protected CounterDisplayer displayer;
     protected Getter getCurrentValue;
     protected int currentlyDisplayedValue;
     protected SingleCoroutine coroutine;
+    protected Fluctuator bounceFluctuator;
 
     public void Initialize(CounterDisplayer displayer, Getter getCurrentValue) {
         this.displayer = displayer;
         this.getCurrentValue = getCurrentValue;
         coroutine = new SingleCoroutine(this);
+        bounceFluctuator = new Fluctuator(this, GetBounceScale, SetBounceScale, bounceCurve);
         UpdateValueInstantly();
     }
 
     public void UpdateValue() {
         coroutine.Start(CUpdateValue());
+        SetBounceScale(bounceScale);
+        bounceFluctuator.GoTo(1.0f, updateDuration);
     }
 
     public void UpdateValueInstantly() {
@@ -40,12 +46,16 @@ public class CounterDisplayerUpdater : MonoBehaviour {
         int currentValue = getCurrentValue();
         startValue = startValue <= currentValue ? Mathf.Min(startValue + 1, currentValue) : Mathf.Max(startValue - 1, currentValue);
         while (!timer.IsOver()) {
-            float avancement = MathCurves.Power(0, 1, timer.GetAvancement(), 1f / 7.0f);
+            float avancement = ApplyCurve(timer.GetAvancement());
             int value = Mathf.CeilToInt(MathCurves.Linear(startValue, currentValue, avancement));
             DisplayScore(value);
             yield return null;
         }
         DisplayScore(currentValue);
+    }
+
+    protected float ApplyCurve(float value) {
+        return MathCurves.Power(0, 1, value, 1f / 7.0f);
     }
 
     protected void DisplayScore(int value) {
@@ -56,5 +66,13 @@ public class CounterDisplayerUpdater : MonoBehaviour {
 
     public string ApplyToCreditsFormating(int value) {
         return useCreditsFormating ? StringHelper.ToCreditsFormat(value) : value.ToString();
+    }
+
+    protected float GetBounceScale() {
+        return displayer.displayText.transform.localScale.x;
+    }
+
+    protected void SetBounceScale(float scale) {
+        displayer.displayText.transform.localScale = Vector3.one * scale;
     }
 }
